c33d60c6dec9eed992647d0f2f4b3f22670a8def
Wesley <wesme1337@openmailbox.org>
2018-04-25 16:15:51 -0400

    first code commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+const config = require("./config.json")
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+	if(newMember.voiceChannel && newMember.voiceChannel.name == 'The bois'){
+		if(newMember.user.username.toLowerCase() == 'mychaall') {
+			let channel = client.channels.find('name', 'The bois');
+			channel.join().then(connection => {
+					let audioFile = 'mychaall' + (Math.floor(Math.random() * 4) + 1) + '.mp3';
+			    const dispatcher = connection.playFile(audioFile);
+			    dispatcher.on('end', () => connection.disconnect());
+			});
+		}
+	}
+});
+
+client.login(config.token);
